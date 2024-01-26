c5d4e8b252f7340c953a946b2b9f217e99e14c3a
Cujo <rene@instinkt.nl>
2024-01-26 17:47:34 +0100

    Added extra logging to see who is winning

--- scripts/rps.js
+++ scripts/rps.js
@@ -5,7 +5,6 @@ let computerwins = 0;
 
 function pickCompChoice() {
      return rockPaperScissors[Math.floor(Math.random() * 3)];
-
 }
 
 function pickPlayersChoice() {
@@ -17,15 +16,21 @@ function game(compChoice, playersChoice) {
      if ((compChoice === "paper" && playersChoice === "rock") || 
         (compChoice === "rock"  && playersChoice === "scissor") ||
         (compChoice === "scissor" && playersChoice === "paper")) {
+          console.log("Computer wins this round!")
           computerwins++;
         } else if (compChoice !== playersChoice) {
           humanwins++;
+          console.log("Human wins this round!")
+        } else {
+          console.log(`This round is a TIE!`)
         }
 }
 
 for (i = 1;i<= 5; i++) {
      compChoice = pickCompChoice();
+     console.log(`Computer picks: ${compChoice}`)
      playersChoice = pickPlayersChoice();
+     console.log(`Player Choose: ${playersChoice}`)
      game(compChoice, playersChoice)
 }
 
